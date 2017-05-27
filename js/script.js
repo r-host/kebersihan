cd4a12a4c39ffb274115ab317c9bd2eed9741ea7
Rudhi Sasmito <rudhisasmito@gmail.com>
2017-05-27 13:48:31 +0700

    files

---
+++ js/script.js
@@ -0,0 +1,120 @@
+$(document).ready(function(){
+
+	"use strict";
+	
+	/* =================================
+	LOADER 
+	=================================== */
+	$(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+	
+	/* =================================
+	NAVBAR 
+	=================================== */
+	jQuery(window).scroll(function () {
+		var top = jQuery(document).scrollTop();
+		var batas = jQuery(window).height();
+
+		if ( top > batas ) {
+			jQuery('.navbar-main').addClass('stiky');
+		}else {
+			jQuery('.navbar-main').removeClass('stiky'); 
+		}
+	});
+	
+	/* =================================
+	BANNER ROTATOR IMAGE 
+	=================================== */
+	$('#slides').superslides({
+		//animation: "fade",
+		play: 5000,
+		slide_speed: 800,
+		pagination: true,
+		hashchange: false,
+		scrollable: true,
+		
+	});
+	
+
+	/* =================================
+	OWL
+	=================================== */
+	
+	var caro = $("#caro");
+	caro.owlCarousel({
+		items: 1,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		loop: true,
+	});
+	
+	var owl = $("#owl-testimony");
+	owl.owlCarousel({
+		autoplay: 5000,
+		stopOnHover: true,
+		margin: 30,
+		items : 2,
+		nav: true,
+		navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+		dots: true,
+		loop: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			1000:{
+				items:2
+			}
+		}
+	});
+
+	var testimony2 = $("#owl-testimony2");
+	testimony2.owlCarousel({
+		autoplay: 5000,
+		stopOnHover: true,
+		margin: 30,
+		items : 3,
+		nav: true,
+		navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+		dots: true,
+		loop: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			1000:{
+				items:3
+			}
+		}
+	});
+	
+	/* =================================
+	FAQ
+	=================================== */	
+	$('.panel-heading a').on('click', function() {
+		$('.panel-heading').removeClass('active');
+		$(this).parents('.panel-heading').addClass('active');
+	});
+	
+	/* =================================
+	MAGNIFIC POPUP
+	=================================== */
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+
+	
+});
+
+
+
+
+  
+  
